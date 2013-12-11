e97d23fdcf4f37ee944cd64b1f3c026716145947
Vadorequest <ambroise.dhenain@gmail.com>
2013-12-11 18:40:39 +0100

    Add video stream FPS.

--- Android/src/com/iha/wcc/job/car/Camera.java
+++ Android/src/com/iha/wcc/job/car/Camera.java
@@ -32,6 +32,11 @@ public class Camera {
      */
     public final static String DEFAULT_CAMERA_HEIGHT = "1200";
 
+    /**
+     * Default FPS used for the camera stream.
+     */
+    public final static String DEFAULT_CAMERA_FPS = "30";
+
     /*
      ******************************************* Methods - Public *****************************************
      */
@@ -53,11 +58,19 @@ public class Camera {
     }
 
     /**
+     * Return the camera FPS to use, use default value as default if settings is not defined.
+     * @return Height.
+     * */
+    public static String getCameraFps(SharedPreferences prefs){
+        return prefs.getString("cameraFps", DEFAULT_CAMERA_HEIGHT);
+    }
+
+    /**
      * Return the command to execute to start the camera video stream using default values.
      * @return Command to execute.
      */
     public static String getCommand(){
-        return "mjpg_streamer -i \"input_uvc.so -d /dev/video0 -r "+DEFAULT_CAMERA_WIDTH+"*"+DEFAULT_CAMERA_HEIGHT+"\" -o \"output_http.so -p 8080 -w /mnt/share\"";
+        return "mjpg_streamer -i \"input_uvc.so -d /dev/video0 -f "+DEFAULT_CAMERA_FPS+" -r "+DEFAULT_CAMERA_WIDTH+"*"+DEFAULT_CAMERA_HEIGHT+"\" -o \"output_http.so -p 8080 -w /mnt/share\"";
     }
 
     /**
@@ -65,6 +78,6 @@ public class Camera {
      * @return Command to execute.
      */
     public static String getCommand(SharedPreferences prefs){
-        return "mjpg_streamer -i \"input_uvc.so -d /dev/video0 -r "+getCameraWidth(prefs)+"*"+getCameraHeight(prefs)+"\" -o \"output_http.so -p 8080 -w /mnt/share\"";
+        return "mjpg_streamer -i \"input_uvc.so -d /dev/video0 -f "+getCameraFps(prefs)+" -r "+getCameraWidth(prefs)+"*"+getCameraHeight(prefs)+"\" -o \"output_http.so -p 8080 -w /mnt/share\"";
     }
 }
